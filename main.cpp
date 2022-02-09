5d50183a6404236331db5f22f0b64528fa7ffd78
TheBlueChameleon <stefan_hartinger@gmx.de>
2022-02-09 20:23:39 +0100

    relaunch under qbs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(int argc, char** argv) {
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
